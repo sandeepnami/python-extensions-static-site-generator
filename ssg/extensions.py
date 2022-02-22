3dedabcf872d9a41c8be6d19c84697a2eeb3c75a
sandeepnami <nami.sandeep@gmail.com>
2022-02-22 14:20:08 +0530

    ssg.py

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -2,3 +2,12 @@ import sys
 import importlib
 
 from pathlib import Path
+
+def load_module(directory, name):
+    sys.path.insert(0, directory)
+    importlib.import_module(name)
+    sys.path.pop(0)
+    
+def load_directory(directory):
+    for path in directory.rglob("*.py"):
+        load_module(directory.as_posx(), name=path.stem)
