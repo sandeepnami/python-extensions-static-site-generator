d4bc6e6e3c77f20821cacfca09ec9a075e7ea814
sandeepnami <nami.sandeep@gmail.com>
2022-02-22 15:03:11 +0530

    ssg.py

--- ssg/hooks.py
+++ ssg/hooks.py
@@ -7,3 +7,14 @@ def register(hook, order=0):
     
     return register_callback
 
+
+def event(hook,*args):
+    for order in sorted(_callbacks.get(hook, {})):
+        for func in _callbacks[hook][order]:
+            func(*args)
+            
+def filter(hook, value, **args):
+    for order in sorted(_callbacks.get(hook, {})):
+        for func in _callbacks[hook][order]:
+            func(value, *args)
+    return value
